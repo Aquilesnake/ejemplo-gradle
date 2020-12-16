d6b128072d029afe642d0645af61d3b85f3c1675
Cristian Tapia <criss.aquiles@gmail.com>
2020-12-16 18:13:29 -0300

    Seleccionador jenkins y creacios de maven.groovy y gradle.groovy v8

--- gradle.groovy
+++ gradle.groovy
@@ -9,7 +9,7 @@ def call (){
     stage('sonar') {
          def scannerHome = tool 'sonar';
              withSonarQubeEnv('sonar') {
-                bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+                bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
              }
     }
         stage('run') {
