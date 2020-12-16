8f537023ff4aad8dece47e414d7c5c93a1391045
Cristian Tapia <criss.aquiles@gmail.com>
2020-12-16 19:00:21 -0300

    Seleccionador jenkins y creacios de maven.groovy y gradle.groovy v15

--- maven.groovy
+++ maven.groovy
@@ -3,9 +3,7 @@
 	def ejecucion = load 'script.groovy'
 	ejecucion.call()
 */
-​
 def call(){
-  
     stage('build'){
         bat 'mvnw.cmd clean compile -e'
     }
@@ -31,7 +29,4 @@ def call(){
     stage('nexus'){
         nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
     }
-​
 }
-​
-
