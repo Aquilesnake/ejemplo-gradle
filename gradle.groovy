f94f3511bdff347d9eb0c068ff31c28ae22d4a42
Cristian Tapia <criss.aquiles@gmail.com>
2020-12-16 18:08:47 -0300

    Seleccionador jenkins y creacios de maven.groovy y gradle.groovy v6

--- gradle.groovy
+++ gradle.groovy
@@ -9,18 +9,18 @@ def call (){
     stage('sonar') {
          def scannerHome = tool 'sonar';
              withSonarQubeEnv('sonar') {
-                bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=maven-gradle -Dsonar.host.url=http://localhost:9000 -Dsonar.login=7cb9a6e8d0c11a1f0a6fc274daa67930a19b8234"
-                }
-            }
+                bat "${scannerHome}/bin/sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build -Dsonar.login=d4167fce436e669acff54aaa33d5b517ecf1f5eb"
+             }
+    }
         stage('run') {
             bat 'start gradlew bootRun &'
             sleep 20
         }
             stage('rest') {
-            bat "curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing"
+            bat 'curl -X GET http://localhost:8082/rest/mscovid/test?msg=testing'
             }
                 stage('nexus') {
                 nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
-               }
+                }
 }
 return this;
