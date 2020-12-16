c270cd0a47c89df3698ea7e6f912c2cb4479e005
Cristian Tapia <criss.aquiles@gmail.com>
2020-12-16 18:48:44 -0300

    Seleccionador jenkins y creacios de maven.groovy y gradle.groovy v11

--- maven.groovy
+++ maven.groovy
@@ -1,49 +1,37 @@
 /*
-forma de invocacion de metodo call; 
-
-def ejecucion = load 'script.groovy'
-ejecucion.call()
-
+	forma de invocación de método call:
+	def ejecucion = load 'script.groovy'
+	ejecucion.call()
 */
-def call (){
-                 stage('compiled') {
-                   steps {
-                       dir('C:\\Users\\AKLSNK\\Desktop\\clases DevOps\\Develoment\\Jenkinsfile\\ejemplo-maven'){
-                                bat './mvnw.cmd clean compile -e'
-                       }
-                   }
-                 }
-               stage('unit') {
-                   steps {
-                       sleep 10
-                       dir('C:\\Users\\AKLSNK\\Desktop\\clases DevOps\\Develoment\\Jenkinsfile\\ejemplo-maven'){
-                              bat './mvnw.cmd clean test -e'
-                       }
-                   }
-               }
-               stage('jar') {
-                   steps {
-                       sleep 10
-                        dir('C:\\Users\\AKLSNK\\Desktop\\clases DevOps\\Develoment\\Jenkinsfile\\ejemplo-maven'){
-                              bat './mvnw.cmd clean package -e'
-                        }
-                   }
-               }
-               stage('run') {
-                   steps {
-                       sleep 20
-                       dir('C:\\Users\\AKLSNK\\Desktop\\clases DevOps\\Develoment\\Jenkinsfile\\ejemplo-maven'){
-                        bat 'start mvnw.cmd spring-boot:run'
-                        }
-                   }
-               }
-              stage('test') {
-                   steps {
-                        dir('C:\\Users\\AKLSNK\\Desktop\\clases DevOps\\Develoment\\Jenkinsfile\\ejemplo-maven'){
-                            sleep 20
-                              bat 'curl -X GET "http://localhost:8081/rest/mscovid/test?msg=testing"'
-                        }
-                   }
-              }
+​
+def call(){
+  
+    stage('build'){
+        bat 'mvnw.cmd clean compile -e'
+    }
+    stage('test'){
+        bat 'mvnw.cmd clean test -e'
+    }
+    stage('Jar') {
+        bat 'mvnw.cmd clean package -e'
+    }
+     stage('sonar') {
+         def scannerHome = tool 'sonar';
+            withSonarQubeEnv('sonar') {
+            bat "${scannerHome}\\bin\\sonar-scanner -Dsonar.projectKey=ejemplo-gradle -Dsonar.java.binaries=build"
+            }
+    }
+    stage('run'){
+        bat 'start mvnw.cmd spring-boot:run'
+    }
+    stage('testing'){
+        sleep 10
+        bat 'curl http://localhost:8082/rest/mscovid/test?msg=testing'
+    }
+    stage('nexus'){
+        nexusPublisher nexusInstanceId: 'nexus', nexusRepositoryId: 'test-nexus', packages: [[$class: 'MavenPackage', mavenAssetList: [[classifier: '', extension: 'jar', filePath: 'build\\libs\\DevOpsUsach2020-0.0.1.jar']], mavenCoordinate: [artifactId: 'DevOpsUsach2020', groupId: 'com.devopsusach2020', packaging: 'jar', version: '0.0.1']]]
+    }
+​
 }
-return this;
+​
+
